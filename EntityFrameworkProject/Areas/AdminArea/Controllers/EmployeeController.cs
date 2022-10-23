1bef3d70a8ef7325352d6268aef772e9a0b1fe7b
Orkhansha <orkhansha@code.edu.az>
2022-10-23 15:07:53 +0400

    Added SliderCrud files

---
+++ EntityFrameworkProject/Areas/AdminArea/Controllers/EmployeeController.cs
@@ -0,0 +1,180 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Employee> employees = await _context.Employees.AsNoTracking().ToListAsync();
+            return View(employees);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SetStatus(int id)
+        {
+            Employee employee = await _context.Employees.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (employee is null) return NotFound();
+
+            if (employee.IsActive)
+            {
+                employee.IsActive = false;
+            }
+            else
+            {
+                employee.IsActive = true;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+
+
+                bool isExist = await _context.Employees.AnyAsync(m => m.FullName.Trim() == employee.FullName.Trim());
+
+                if (isExist)
+                {
+                    ModelState.AddModelError("Name", "Employee already exist");
+                    return View();
+                }
+
+                
+
+                await _context.Employees.AddAsync(employee);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            Employee employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null) return NotFound();
+
+            return View(employee);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Employee employee = await _context.Employees.FirstOrDefaultAsync(m => m.Id == id);
+
+            _context.Employees.Remove(employee);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            try
+            {
+                if (id is null) return BadRequest();
+
+                Employee employee = await _context.Employees.FirstOrDefaultAsync(m => m.Id == id);
+
+                if (employee is null) return NotFound();
+
+                return View(employee);
+
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Employee employee)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(employee);
+                }
+
+                Employee dbEmployee = await _context.Employees.AsNoTracking().FirstOrDefaultAsync(m => m.Id == id);
+
+                if (dbEmployee is null) return NotFound();
+
+                if (dbEmployee.FullName.ToLower().Trim() == employee.FullName.ToLower().Trim())
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                //dbCategory.Name = category.Name;
+
+                _context.Employees.Update(employee);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+    }
+}
